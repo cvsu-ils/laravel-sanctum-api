388e5c2b011f6968530abccb6f962e04d612d8ec
John Vincent Bonza <jayveeinfinity@gmail.com>
2023-10-25 17:28:48 +0800

    Initial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function user()
+    {
+        return response()->json(['user' => Auth::user()]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json(['token' => $token]);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
